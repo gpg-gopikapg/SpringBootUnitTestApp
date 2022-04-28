2ae4deea854ed8a4872f459eebb46f7c3072bab2
GopikaPG <GopikaPG@DESKTOP-ND8AUF3>
2022-04-28 13:10:47 +0530

    Exception Handling added

--- SpringBootUnitTestApp/src/main/java/com/example/demo/controller/EmployeeController.java
+++ SpringBootUnitTestApp/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
@@ -25,33 +25,38 @@ public class EmployeeController {
 	IEmployeeService service;
 
 	@PostMapping("/save")
-	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
-		return new ResponseEntity<Employee>(service.saveEmployee(employee), HttpStatus.CREATED);
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Employee saveEmployee(@RequestBody Employee employee) {
+		return service.saveEmployee(employee);
 	}
 
 	@GetMapping("/all")
-	public ResponseEntity<List<Employee>> getAllEmployees() {
-		return new ResponseEntity<List<Employee>>(service.getAllEmployees(), HttpStatus.OK);
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<Employee> getAllEmployees() {
+		return service.getAllEmployees();
 	}
 
 	@GetMapping("/get/{id}")
-	public ResponseEntity<Employee> getEmployee(@PathVariable Integer id) {
-		return new ResponseEntity<Employee>(service.getEmployee(id), HttpStatus.OK);
+	@ResponseStatus(value = HttpStatus.OK)
+	public Employee getEmployee(@PathVariable Integer id) {
+		return service.getEmployee(id);
 	}
 
 	@DeleteMapping("delete/{id}")
-	public ResponseEntity<Void> deleteEmployee(@PathVariable Integer id) {
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void deleteEmployee(@PathVariable Integer id) {
 		service.deleteEmployee(id);
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+
 	}
 
 	@PutMapping("/update/{id}")
-	public ResponseEntity<Employee> updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
+	@ResponseStatus(value = HttpStatus.OK)
+	public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee employee) {
 		Employee emp = service.getEmployee(id);
 		emp.setEmpName(employee.getEmpName());
 		emp.setEmpSalary(employee.getEmpSalary());
 		service.saveEmployee(emp);
-		return ResponseEntity.ok(emp);
+		return emp;
 
 	}
 
